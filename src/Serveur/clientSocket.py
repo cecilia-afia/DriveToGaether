d4e3e4b7544538ef56f83fe7aa0abdbc5738f34c
LeDirge <valentinandral@hotmail.fr>
2023-05-03 13:43:27 +0200

    Plus robuste, est censé permettre l'envoi vers un serveur distant

--- src/Serveur/clientSocket.py
+++ src/Serveur/clientSocket.py
@@ -1,26 +1,41 @@
 import socket
 MAX_SIZE = 2048
 
-def client_socket():
-    host = socket.gethostname()  # only works when the server and the client are working on the same machine
-    port = int(input("Chose a port number: (careful, the port number must be the same as the server's one!)"))  # socket server port number
-    if port == "" or port < 1024:
-        port = 5000
+def client_function():
+    #host = socket.gethostname()  # only works when the server and the client are working on the same machine
+    #host = input("Choose the name of the host (you can find it once the server is launched) : ")
+    host = "25.22.85.243"
+    port = input("Choose a port number (you can find it once the server is launched): ")  # socket server port number
+
+    print(host)
+    ## TESTING PORT ##
+    if port == '':
+        port = 5000 # arbitrary number, was used during previous experiment
+        print("Port initialized with 5000: can't be NULL")
+
+    elif int(port) < 1024:
+        port = 5000 # arbitrary number, was used during previous experiment
+        print("Port initialized with 5000: port can't be >= 1024")
+
+    
+    port = int(port)
+    ## END TESTING PORT ##
+
     client_socket = socket.socket()  # instantiate
-    client_socket.connect((host, port))  # connect to the server
+    client_socket.connect((host, port))  # try to connect to the server
 
-    message = input("Message à transmettre : ")  # take input
+    message = input("Message to be transmitted : ")  # first message that'll be send
 
-    while message.lower().strip() != 'quit':
+    while message.lower().strip() != 'quit': # better than a devastating Ctrl + C 
         client_socket.send(message.encode())  # send message
         data = client_socket.recv(MAX_SIZE).decode()  # receive response
 
         print('Received from server: ' + data)  # show in terminal
 
-        message = input(" Message à transmettre ")  # again take input
+        message = input(" Message to be transmitted : ")  # send a new message
 
     client_socket.close()  # close the connection
 
 
 if __name__ == '__main__':
-    client_socket()
+    client_function()
