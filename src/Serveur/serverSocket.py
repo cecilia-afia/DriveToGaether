6586a0699ceccddbbf7670c5ea4092e1333717a0
LeDirge <valentinandral@hotmail.fr>
2023-05-03 13:53:52 +0200

    Plus robuste, est censée recevoir depuis un client distant

--- src/Serveur/serverSocket.py
+++ src/Serveur/serverSocket.py
@@ -2,17 +2,33 @@ import socket
 nb_players = 10
 MAX_SIZE = 2048
 
-def server_socket():
-    host = socket.gethostname() #get the name of host's machine
-    port = int(input("Chose a port number : ")) # arbitrary number, was used during previous experiment
-    if port == "" or port < 1024:
-        port = 5000
+def server_function():
+    # host = socket.gethostname() #get the name of host's machine
+    host = "25.22.85.243"
+    print("hostname = %s"%host)
+
+    port = input("Choose a port number : ")
+
+    ## TESTING PORT ##
+    if port == '':
+        port = 5000 # arbitrary number, was used during previous experiment
+        print("Port initialized with 5000: can't be NULL")
+
+    elif int(port) < 1024:
+        port = 5000 # arbitrary number, was used during previous experiment
+        print("Port initialized with 5000: port can't be >= 1024")
+
+    
+    port = int(port)
+    ## END TESTING PORT ##
+
     server_socket = socket.socket()  # create the socket
     server_socket.bind((host, port))  # bind host address and port together
 
     server_socket.listen(nb_players) # the server will be able to listen up to <nb_players> connections
     conn, address = server_socket.accept()  # accept new connection
     print("Connection from: " + str(address))
+
     while True:
         data = conn.recv(MAX_SIZE).decode()
         if not data:
@@ -25,4 +41,4 @@ def server_socket():
 
 
 if __name__ == '__main__':
-    server_socket()
+    server_function()
